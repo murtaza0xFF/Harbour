222705ccab4795231c73f8e614327a6938388910
murtaza0xff <murtazasakbari@gmail.com>
2019-03-21 14:09:23 +0530

    Populate AlgoliaApi class with search functions

--- algolia/src/main/java/com/murtaza0xff/harbour/algolia/AlgoliaApi.kt
+++ algolia/src/main/java/com/murtaza0xff/harbour/algolia/AlgoliaApi.kt
@@ -1,9 +1,21 @@
 package com.murtaza0xff.harbour.algolia
 
+import com.murtaza0xff.harbour.algolia.model.MatchingComment
 import com.murtaza0xff.harbour.algolia.model.MatchingStory
+import com.murtaza0xff.harbour.algolia.model.MatchingUrl
 import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlgoliaApi {
 
-    fun getMatchingStories(): Flowable<MatchingStory>
+    @GET("/search?tags=story")
+    fun getMatchingStories(@Query("query") query: String): Flowable<MatchingStory>
+
+    @GET("/search?tags=comment")
+    fun getMatchingComments(@Query("query") query: String): Flowable<MatchingComment>
+
+    @GET("/search?restrictSearchableAttributes=url")
+    fun getMatchingUrl(@Query("query") query: String): Flowable<MatchingUrl>
+
 }
--- algolia/src/main/java/com/murtaza0xff/harbour/algolia/model/MatchingStory.kt
+++ algolia/src/main/java/com/murtaza0xff/harbour/algolia/model/MatchingStory.kt
@@ -1,14 +1,6 @@
 package com.murtaza0xff.harbour.algolia.model
 
 data class MatchingStory(
-    val exhaustiveNbHits: Boolean,
-    val hits: List<StoryHit>,
-    val hitsPerPage: Int,
-    val nbHits: Int,
-    val nbPages: Int,
-    val page: Int,
-    val params: String,
-    val processingTimeMS: Int,
-    val query: String
+    val hits: List<StoryHit>
 )
 
